aee20ef712a4e779f202ac18cee44c0ec3e7c1e8
sachin1830 <sachinkumarbiswal18@gmail.com>
2021-01-20 23:29:34 +0530

    HQL

--- hibernateproject/src/main/java/com/hibernate/Certificate.java
+++ hibernateproject/src/main/java/com/hibernate/Certificate.java
@@ -13,10 +13,18 @@ public class Certificate
 	public void setCourse(String course) {
 		this.course = course;
 	}
+	public Certificate(String course, String duration) {
+		super();
+		this.course = course;
+		this.duration = duration;
+	}
 	public String getDuration() {
 		return duration;
 	}
 	public void setDuration(String duration) {
 		this.duration = duration;
+	}
+	public Certificate() {
+		super();
 	}
 }
---
+++ hibernateproject/src/main/java/com/mapmm/Employee.java
@@ -0,0 +1,45 @@
+package com.mapmm;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Employee 
+{
+	@Id
+	@Column(name = "emp_id")
+	private int empId;
+	@Column(name = "emp_name")
+	private String empName;
+	
+	@ManyToMany
+	private List<Project> projects;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+}
